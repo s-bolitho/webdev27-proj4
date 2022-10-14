eef8884a83fd03c783c8bb313ee6eca0601c589e
s-bolitho <sambolitho4@gmail.com>
2022-10-13 21:31:55 -0500

    part 3 done

--- server/middleware/isAuthenticated.js
+++ server/middleware/isAuthenticated.js
@@ -5,8 +5,11 @@ const {SECRET} = process.env
 module.exports = {
     //isAuthenticated making sure that user is authenticated lol (logged in)
     isAuthenticated: (req, res, next) => {
-        const headerToken = req.get('Authorization')
 
+        //headerToken getting authorization for logged in user
+        const headerToken = req.get('Authorization')
+        
+        //"if (!headerToken)" sends back error if trying to go to page for only authenticated users
         if (!headerToken) {
             console.log('ERROR IN auth middleware')
             res.sendStatus(401)
@@ -14,13 +17,15 @@ module.exports = {
 
         let token
 
+        //"try" making sure that user is logged in
         try {
             token = jwt.verify(headerToken, SECRET)
         } catch (err) {
             err.statusCode = 500
             throw err
         }
-
+        
+        //"if (!token)" sending an error if user isn't logged in
         if (!token) {
             const error = new Error('Not authenticated.')
             error.statusCode = 401
